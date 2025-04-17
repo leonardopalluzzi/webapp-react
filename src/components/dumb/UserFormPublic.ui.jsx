53edc0ded31e23e8c37ba00b5122bdb428a17780
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-04-17 15:05:15 +0200

    Refactor RegisterUi and styles for improved layout and user experience

--- src/components/dumb/UserFormPublic.ui.jsx
+++ src/components/dumb/UserFormPublic.ui.jsx
@@ -1,19 +1,24 @@
 export default function RegisterUi({ onSubmit, onChangeUsername, onChangePassword, username, password, title, button, esit }) {
     return (
         <>
-            <div className="container">
-                <h1>{title}</h1>
+            <div className="container_form " data-bs-theme="dark">
+                <div className="card w-50">
+                    <div className="card-header">
+                        <h1>{title}</h1>
+                    </div>
+                    <div className="card-body">
+                        <form className="public_form" onSubmit={(e) => { e.preventDefault(); onSubmit() }} method="POST">
+                            <input value={username} onChange={(e) => onChangeUsername(e.target.value)} className="form-control" type="text" placeholder="username" />
+                            <input value={password} onChange={(e) => onChangePassword(e.target.value)} className="form-control" type="text" placeholder="password" />
+                            <button type="submit" className="btn btn-primary" >{button}</button>
+                        </form>
+                        <div className={esit.state == 'success' ? `text-success` : 'text-danger'}>
+                            <h6>{esit.state}</h6>
+                            <span>{esit.message}</span>
+                        </div>
+                    </div>
 
-                <form onSubmit={(e) => { e.preventDefault(); onSubmit() }} method="POST">
-                    <input value={username} onChange={(e) => onChangeUsername(e.target.value)} className="form-control" type="text" placeholder="username" />
-                    <input value={password} onChange={(e) => onChangePassword(e.target.value)} className="form-control" type="text" placeholder="password" />
-                    <button type="submit" className="btn btn-primary" >{button}</button>
-                </form>
-                <div className={esit.state == 'success' ? `text-success` : 'text-danger'}>
-                    <h6>{esit.state}</h6>
-                    <span>{esit.message}</span>
                 </div>
-
             </div >
         </>
     )
