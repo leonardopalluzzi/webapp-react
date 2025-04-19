c2d5ac08d18b4e2d1b2af52e164e6223bc063ac6
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-04-19 21:04:30 +0200

    Remove hardcoded admin credentials from Jumbo component and clean up Home component layout

--- src/components/dumb/Jumbo.ui.jsx
+++ src/components/dumb/Jumbo.ui.jsx
@@ -6,6 +6,11 @@ export default function JumboUi({ image, title, content }) {
                     <img className="jumbo_img" src={`http://localhost:3000/${image}`} alt="" />
                 </div>
                 <div className="jumbo_info">
+                    <span className="text-danger fs-3 bg-dark-subtle">
+                        per loggare con utenza admin: <br />
+                        username: leonardo <br />
+                        password: leo1234pallu
+                    </span>
                     <h1>{title}</h1>
                     <span>{content}</span>
                 </div>
