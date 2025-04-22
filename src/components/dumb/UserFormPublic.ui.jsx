f839f56c8e70215a1bc37e38bd70b4d70e529644
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-04-22 12:13:10 +0200

    Fix input autocomplete attribute in UserFormPublic and update AdminRoute to use currentUser for authentication checks

--- src/components/dumb/UserFormPublic.ui.jsx
+++ src/components/dumb/UserFormPublic.ui.jsx
@@ -21,7 +21,7 @@ export default function RegisterUi({ onSubmit, onChangeUsername, onChangePasswor
                     <div className="card-body">
                         <form className="public_form" onSubmit={(e) => { e.preventDefault(); onSubmit() }} method="POST">
                             <input value={username} onChange={(e) => onChangeUsername(e.target.value)} className="form-control" type="text" placeholder="username" required />
-                            <input value={password} onChange={(e) => onChangePassword(e.target.value)} className="form-control" type="password" placeholder="password" autocomplete="current-password" required />
+                            <input value={password} onChange={(e) => onChangePassword(e.target.value)} className="form-control" type="password" placeholder="password" autoComplete="current-password" required />
                             <button onClick={() => navigate(redirect)} type="submit" className="btn btn-primary" >{button}</button>
                         </form>
                         <div className={esit.state == 'success' ? `text-success` : 'text-danger'}>
--- src/components/smart/AdminRoute.jsx
+++ src/components/smart/AdminRoute.jsx
@@ -1,18 +1,17 @@
 import { Navigate } from 'react-router-dom';
-import { useAuthContext } from '../../contexts/authenticationContext'
 
 export default function AdminRoute({ children, requiredRole }) {
 
-    const { loginEsit } = useAuthContext()
-    console.log(loginEsit);
+    const currentUser = JSON.parse(localStorage.getItem('user'))
+    console.log(currentUser);
 
 
-    if (!loginEsit) {
+    if (!currentUser) {
         // utente non loggato
         return <Navigate to='/login' replace />
     }
 
-    if (requiredRole !== undefined && loginEsit.role !== requiredRole) {
+    if (requiredRole !== undefined && currentUser.role !== requiredRole) {
         //utente loogato ma non admin
         return <Navigate to='/login' replace />
     }
